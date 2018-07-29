95fb24f30e690d903d45a488b5032c16b40d7efe
ZK_XU <xuzhaokang19943@hotmail.com>
2018-07-29 05:35:22 -0400

    士兵anim好了。prefab文件夹移出去了

---
+++ Project/GameOriginalScheme/Assets/Scripts/ArchorAnim.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ArchorAnim : MonoBehaviour {
+	PlayerController playerController;
+	Animator animator;
+	// Use this for initialization
+	void Start () {
+		GameObject King = GameObject.Find ("King");
+		playerController = King.GetComponent<PlayerController> ();
+		animator = GetComponent<Animator> ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		animator.SetFloat ("WalkX", Input.GetAxisRaw("Horizontal"));
+		animator.SetFloat ("WalkY", Input.GetAxisRaw("Vertical"));
+		animator.SetBool ("PlayerMoving", playerController.playerMoving);
+		animator.SetFloat ("LastMoveX", playerController.lastMove.x);
+		animator.SetFloat ("LastMoveY", playerController.lastMove.y);     
+	}
+}
