622706c59bc92153a98fff6e4f0e86d8675cd287
ZK_XU <xuzhaokang19943@hotmail.com>
2018-08-17 06:34:51 -0400

    revert

--- Project/GameOriginalScheme/Assets/Scripts/AOEAttack.cs
+++ Project/GameOriginalScheme/Assets/Scripts/AOEAttack.cs
@@ -59,6 +59,7 @@ public class AOEAttack : MonoBehaviour {
 		RotateAroundPivot(direction, transform);
 
 <<<<<<< HEAD
+<<<<<<< HEAD
 		SoundManager.instance.PlaySound("generalAttack");
 
 <<<<<<< HEAD
@@ -81,6 +82,10 @@ public class AOEAttack : MonoBehaviour {
 				enemiesToDamage [i].GetComponent<PullBar> ().StateChange ();
 			}
 
+=======
+		for (int i = 0; i < enemiesToDamage.Length; i++) {
+			enemiesToDamage [i].GetComponent<CharacterHealth> ().TakeDamage (damage);
+>>>>>>> parent of 0c7c0df... 机关控制的移动平台
 =======
 		for (int i = 0; i < enemiesToDamage.Length; i++) {
 			enemiesToDamage [i].GetComponent<CharacterHealth> ().TakeDamage (damage);
