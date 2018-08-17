e8b3714899de563e0889796598874a0666d0e141
ZK_XU <xuzhaokang19943@hotmail.com>
2018-08-17 06:39:34 -0400

    Revert "添加开始界面素材"
    This reverts commit e23f5e0f0179947827ceb5fe1ed37ecbdbe458f9.

--- Project/GameOriginalScheme/Assets/Scripts/AOEAttack.cs
+++ Project/GameOriginalScheme/Assets/Scripts/AOEAttack.cs
@@ -58,11 +58,8 @@ public class AOEAttack : MonoBehaviour {
 
 		RotateAroundPivot(direction, transform);
 
-<<<<<<< HEAD
-<<<<<<< HEAD
 		SoundManager.instance.PlaySound("generalAttack");
 
-<<<<<<< HEAD
 		if (m_bingAni)
 		{
 			PlayAnimation(m_direction);
@@ -79,21 +76,9 @@ public class AOEAttack : MonoBehaviour {
 			if (enemiesToDamage[i].tag != "Device") {
 				enemiesToDamage [i].GetComponent<CharacterHealth> ().TakeDamage (damage);
 			}else {
-				enemiesToDamage [i].GetComponent<PullBar> ().StateChange ();
+				//enemiesToDamage [i].GetComponent<PullBar> ().StateChange ();
 			}
 
-=======
-		for (int i = 0; i < enemiesToDamage.Length; i++) {
-			enemiesToDamage [i].GetComponent<CharacterHealth> ().TakeDamage (damage);
->>>>>>> parent of 0c7c0df... 机关控制的移动平台
-=======
-		for (int i = 0; i < enemiesToDamage.Length; i++) {
-			enemiesToDamage [i].GetComponent<CharacterHealth> ().TakeDamage (damage);
->>>>>>> parent of 0c7c0df... 机关控制的移动平台
-=======
-		for (int i = 0; i < enemiesToDamage.Length; i++) {
-			enemiesToDamage [i].GetComponent<CharacterHealth> ().TakeDamage (damage);
->>>>>>> parent of 0c7c0df... 机关控制的移动平台
 		}
 
 		coolDownTime = startTime;
