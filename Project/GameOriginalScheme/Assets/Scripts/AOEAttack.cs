ed5934697ec5de51756605920efa5aba9e92d35e
Jassie <jassie0@sina.cn>
2018-08-17 19:50:48 +0800

    Revert "revert"
    This reverts commit 622706c59bc92153a98fff6e4f0e86d8675cd287.

--- Project/GameOriginalScheme/Assets/Scripts/AOEAttack.cs
+++ Project/GameOriginalScheme/Assets/Scripts/AOEAttack.cs
@@ -59,7 +59,6 @@ public class AOEAttack : MonoBehaviour {
 		RotateAroundPivot(direction, transform);
 
 <<<<<<< HEAD
-<<<<<<< HEAD
 		SoundManager.instance.PlaySound("generalAttack");
 
 <<<<<<< HEAD
@@ -82,10 +81,6 @@ public class AOEAttack : MonoBehaviour {
 				enemiesToDamage [i].GetComponent<PullBar> ().StateChange ();
 			}
 
-=======
-		for (int i = 0; i < enemiesToDamage.Length; i++) {
-			enemiesToDamage [i].GetComponent<CharacterHealth> ().TakeDamage (damage);
->>>>>>> parent of 0c7c0df... 机关控制的移动平台
 =======
 		for (int i = 0; i < enemiesToDamage.Length; i++) {
 			enemiesToDamage [i].GetComponent<CharacterHealth> ().TakeDamage (damage);
