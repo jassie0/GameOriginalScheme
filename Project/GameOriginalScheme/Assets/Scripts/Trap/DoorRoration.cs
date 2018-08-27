4caec26d64d4b404fc49e7b086fa4e0ab9311927
ZK_XU <xuzhaokang19943@hotmail.com>
2018-08-27 11:31:35 -0400

    踏板控制门开关

--- Project/GameOriginalScheme/Assets/Scripts/Trap/DoorRoration.cs
+++ Project/GameOriginalScheme/Assets/Scripts/Trap/DoorRoration.cs
@@ -3,14 +3,35 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class DoorRoration : MonoBehaviour {
-
+	Animator colliderAnim;
+	Animator doorAnim;
+	public Transform doorCollider;
+	public GameObject doorSprite;
+	public GameObject[] activator;
+	bool doorOpen;
 	// Use this for initialization
 	void Start () {
-		
+		colliderAnim = doorCollider.GetComponent<Animator> ();
+		doorAnim = doorSprite.GetComponent<Animator> ();
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		
+		for (int i = 0; i < activator.Length; i++) {
+			if (activator[i].GetComponent<MachineTrigger> ().machineOn == true) {
+				if (!doorOpen) {
+					colliderAnim.SetTrigger ("OpenDoor");
+					doorAnim.SetTrigger ("Open");
+					activator [i].GetComponent<MachineTrigger> ().machineOn = false;
+					doorOpen = true;
+				} else {
+					colliderAnim.SetTrigger ("CloseDoor");
+					doorAnim.SetTrigger ("Close");
+					activator [i].GetComponent<MachineTrigger> ().machineOn = false;
+					doorOpen = false;
+				}
+			} 
+		}
 	}
+		
 }
